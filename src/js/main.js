c8af336eaefca59081aa9ba9e93a3c1c467273ae
Artem Savienkov <savienkov.ao@gmail.com>
2017-05-02 02:35:29 +0300

    burger-menu and slider script were added

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,129 @@
-$(function() {
-// page scroll animation
+(function() {
 
-	$("#header-menu").on("click","a", function (event) {
-		
-		var id = $(this).attr('href');
-		
-		if ( id.match(/#\w/g) ){
-			event.preventDefault();
-			var top = $(id).offset().top;
+// burger menu toggle
+	var brgrMenu = document.getElementById("brgr-menu");
+	var menu = document.getElementById("header-menu");
+	var count = 0;
+	
+	brgrMenu.addEventListener("click", enableBrgrMenu, false);
+
+	function enableBrgrMenu(event){
+		var e = getTarget(event);
+
+		if (e != undefined) {
+			count++;
+			if(count%2==0) {
+				deactivateBrgrMenu();				
+			} else {
+				activateBrgrMenu();
+			}
+		}
+	}
+
+	function activateBrgrMenu(){
+		menu.style.display = 'block';
+		brgrMenu.classList.add('active');		
+	}
+
+	function deactivateBrgrMenu(){
+		menu.style.display = '';
+		brgrMenu.classList.remove('active');
+		count = 0;
+	}
+
+	function getTarget(event) {
+		var target = event.target;
+		var currentTarget = event.currentTarget;
+
+		while (target != event.currentTarget) {
+
+			if (target.hasAttribute('data-meta-node')) {
+				var e = {target, currentTarget};
+				
+				return e;
+			}
+			target = target.parentNode;
+		}
+		return;
+	}
+
+// age change
+	var age = document.getElementById('age');
+	var year = new Date();
+	var birthday = new Date(1992,09,13);
+	age.innerHTML = Math.floor( (year - birthday) / (1000 * 60 * 60 * 24 * 365) );
+
+// experience block slider
+	var slider = document.getElementById('slider');
+	var slInner = document.getElementById('slider__inner');
+	var slides = document.querySelectorAll('.slider-item');
+	var firstSlide = document.querySelectorAll('.slider-item')[0];
+	var arrLeft = document.getElementById('slider-arrw-l');
+	var arrRight = document.getElementById('slider-arrw-r');
+	var slItemsNumber = slides.length;
+	var slItemMargin = 0;
+	var prevSliderWidth;
+
+	arrLeft.addEventListener("click", browse, false);
+	arrRight.addEventListener("click", browse, false);
+	window.addEventListener("resize", browse, false);
+
+	function browse() {
+		var sliderWidth = slider.clientWidth;
+		var browseCount = slItemsNumber - 1;
+
+		if(this != window) {			
+			var direct = this.getAttribute('data-direct');
 		}
 
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+		if (direct == 'left') {				
+			if(slItemMargin < 0){
+				slItemMargin += sliderWidth;
+				firstSlide.style.marginLeft = slItemMargin + 'px';
+			}
+		} else if ( direct == 'right'){
+			slItemMargin -= sliderWidth;
+			if (slItemMargin <= 0 && slItemMargin >= -browseCount * sliderWidth){
+				firstSlide.style.marginLeft = slItemMargin + 'px';
+			}
+		}	else {
+			// resize
+			slItemMargin *= (sliderWidth / prevSliderWidth);
+			firstSlide.style.marginLeft = slItemMargin + 'px';
+		}
+
+		if (slItemMargin <= -browseCount * sliderWidth){
+			slItemMargin = -browseCount * sliderWidth;
+		}
+
+		prevSliderWidth = sliderWidth;
+	}
+
+
+
+
+
+
+
+
+
+
+
+// page scroll animation
+
+	// $("#header-menu").on("click","a", function (event) {
+		
+	// 	var id = $(this).attr('href');
+		
+	// 	if ( id.match(/#\w/g) ){
+	// 		event.preventDefault();
+	// 		var top = $(id).offset().top;
+	// 	}
+
+	// 	$('body,html').animate({scrollTop: top}, 1500);
+	// });
 
 
 
 	
-});
+})();
